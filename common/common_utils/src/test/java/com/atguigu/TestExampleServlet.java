16f6ab1cc770a940bd239b65f2e47b11ddca3992
dell <1427011919@qq.com>
2022-09-04 10:46:59 +0800

    第一次提交

---
+++ common/common_utils/src/test/java/com/atguigu/TestExampleServlet.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+import junit.framework.TestCase;
+
+/**
+ * Testing {@link com.atguigu.ExampleMacro}
+ */
+public class TestExampleServlet extends TestCase
+{
+    public void testBasic()
+    {
+        // add test here...
+    }
+}
